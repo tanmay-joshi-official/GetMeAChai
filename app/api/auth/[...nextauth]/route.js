d057f301d88d372487aae6aabec1c06f21afadfe
Tanmay Joshi <tanmayjoshi579@gmail.com>
2026-02-22 15:06:27 +0530

    Created database using mongoose for storing user details

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,39 @@
 import NextAuth from "next-auth"
 import GitHubProvider from "next-auth/providers/github"
+import mongoose from "mongoose"
+import connectDB from "@/app/db/connectDb"
+import User from "@/app/models/User"
+import Payment from "@/app/models/Payment"
 
 const handler = NextAuth({
-    // Configure one or more authentication providers
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
         }),
-        // ...add more providers here
-    ],
+    ],  
+    callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+            if (account.provider === "github") {
+                // Check if the user already exists in the database
+                const currentUser = await User.findOne({ email: user.email })
+                if (!currentUser) {
+                    // Create a new user if they don't exist
+                    const newUser = await User.create({
+                        email: user.email,
+                        username: user.email.split("@")[0]
+                    })
+                }
+                return true
+            }
+        },
+        async session({ session, token, user }) {
+            const dbUser = await User.findOne({ email: session.user.email })
+            console.log(dbUser)
+            session.user.name = dbUser.username
+            return session
+        }
+    }
 })
 
 export { handler as GET, handler as POST }
--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -20,13 +20,13 @@ const Navbar = () => {
             {!session && <Link href="/login">
               <button className="cursor-pointer px-4 p-2 rounded-full bg-linear-to-r from-purple-800 to-teal-800 hover:scale-105 transition-all hover:bg-linear-to-r hover:from-purple-950 hover:to-teal-900 active:bg-linear-to-r active:from-purple-950 active:to-teal-950">Login</button>
             </Link>}
-            {session && <div className="dropdown relative" onClick={() => setdropdown(!dropdown)} onBlur={() => setTimeout(() => setdropdown(false), 100)}>
+            {session && <div className="dropdown relative" onClick={() => setdropdown(!dropdown)}>
               <button className="cursor-pointer px-4 p-2 rounded-full bg-linear-to-r from-purple-800 to-teal-800 hover:scale-105 transition-all hover:bg-linear-to-r hover:from-purple-950 hover:to-teal-900 active:bg-linear-to-r active:from-purple-950 active:to-teal-950 flex items-center gap-2">Profile <IoMdArrowDropdown />
-                <ul className={`options absolute top-12 mx-auto w-46 bg-slate-800 rounded-md shadow-lg dropdown-menu items-start ${dropdown ? 'block' : 'hidden'}`}>
+                <ul className={`options absolute top-12 mx-auto w-46 bg-slate-800 rounded-md shadow-lg dropdown-menu ${dropdown ? 'block' : 'hidden'}`}>
                   <li className="px-4 m-2 my-0 py-2">Signed in as {session.user.name}</li>
                   <Link href="/dashboard"><li className="rounded-lg m-2 my-0 px-4 py-2 hover:bg-slate-700 active:bg-slate-800 transition-all cursor-pointer">Dashboard</li></Link>
-                  <li className="rounded-lg m-2 my-0 px-4 py-2 hover:bg-slate-700 active:bg-slate-800 transition-all cursor-pointer">Settings</li>
-                  <li className="rounded-lg m-2 my-0 px-4 py-2 hover:bg-slate-700 active:bg-slate-800 transition-all cursor-pointer" onClick={() => signOut()}>Sign Out</li>
+                  <Link href={`/${session.user.name}`}><li className="rounded-lg m-2 my-0 px-4 py-2 hover:bg-slate-700 active:bg-slate-800 transition-all cursor-pointer">Your Page</li></Link>
+                  <li className="rounded-lg m-2 my-0 px-4 py-2 hover:bg-slate-700 active:bg-slate-800 transition-all cursor-pointer mb-2" onClick={() => signOut()}>Sign Out</li>
                 </ul></button>
             </div>}
             {session && <button className="cursor-pointer px-4 p-2 rounded-full bg-linear-to-r from-purple-800 to-teal-800 hover:scale-105 transition-all hover:bg-linear-to-r hover:from-purple-950 hover:to-teal-900 active:bg-linear-to-r active:from-purple-950 active:to-teal-950" onClick={() => signOut()}>Sign out</button>}
