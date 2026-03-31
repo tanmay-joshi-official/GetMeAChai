228863dc96d347498cb4a19df60ad205d2967f1f
Tanmay Joshi <tanmayjoshi579@gmail.com>
2026-03-31 22:05:29 +0530

    Updated

--- app/db/connectDb.js
+++ app/db/connectDb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/GetMeAChai")
+        await mongoose.connect(process.env.MONGODB_URI)
     } catch (error) {
         console.error("MongoDB connection failed:", error)
     }
